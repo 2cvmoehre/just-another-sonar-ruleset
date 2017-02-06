3ca325dbe765aea4cf545d44da737aaaea6b2c01
2cvmoehre <datadmin@lubuntu64>
2017-02-07 00:49:46 +0100

    remove template classes

--- src/main/java/org/sonar/template/java/checks/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-package org.sonar.template.java.checks;
-
-/**
- * This package contains the custom rules
- */
-import javax.annotation.ParametersAreNonnullByDefault;
